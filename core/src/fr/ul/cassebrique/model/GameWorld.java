6d381d56999ec988f849a6f7762fca6850246046
Noah Willaime <noah.willaime9@etu.univ-lorraine.fr>
2018-02-21 10:41:06 +0100

    Rebond (sauf racket) OK

--- core/src/fr/ul/cassebrique/model/GameWorld.java
+++ core/src/fr/ul/cassebrique/model/GameWorld.java
@@ -3,9 +3,18 @@ package fr.ul.cassebrique.model;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.WorldManifold;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 import fr.ul.cassebrique.dataFactories.TextureFactory;
 import fr.ul.cassebrique.views.GameScreen;
@@ -27,6 +36,46 @@ public class GameWorld {
 
     public GameWorld(GameScreen screen){
         world = new World(new Vector2(0, 0), true);
+        world.setContactListener(new ContactListener() {
+            @Override
+            public void beginContact(Contact contact) {
+                WorldManifold wm = contact.getWorldManifold();
+                Vector2 normal = wm.getNormal();
+                Body obj1 = contact.getFixtureA().getBody();
+                Body obj2 = contact.getFixtureB().getBody();
+                if (obj2.getType() == BodyDef.BodyType.DynamicBody){
+                    float scalp;
+                    float restitution = obj1.getFixtureList().get(0).getRestitution();
+                    System.out.println(restitution);
+                    Vector2 r = new Vector2();
+                    Vector2 vitesse = obj2.getLinearVelocity();
+                    scalp =-2*(vitesse.x * normal.x + vitesse.y * normal.y); //-2(d . n)
+                    //-2(d . n)n
+                    r.x = scalp * normal.x;
+                    r.y = scalp * normal.y;
+                    //-2(d . n)n + d
+                    r.x = (vitesse.x + r.x) * restitution;
+                    r.y = (vitesse.y + r.y) * restitution;
+                    //On applique r a la ball
+                    obj2.setLinearVelocity(r);
+                }
+            }
+
+            @Override
+            public void endContact(Contact contact) {
+
+            }
+
+            @Override
+            public void preSolve(Contact contact, Manifold oldManifold) {
+
+            }
+
+            @Override
+            public void postSolve(Contact contact, ContactImpulse impulse) {
+
+            }
+        });
         gs = screen;
         wall = new Wall(this);
         racket = new Racket(this);
@@ -49,10 +98,10 @@ public class GameWorld {
         balls.add(new Ball(this, pos2));
         Vector2 pos3 = new Vector2();
         pos3.x = getRacket().getPos().x + getRacket().getWidth()/2;
-        pos3.y = getRacket().getPos().y + getRacket().getHeight();
+        pos3.y = getRacket().getPos().y + getRacket().getHeight()+10;
         pos3.y += TextureFactory.getTexBall().getHeight()/2;
         balls.add(new Ball(this, pos3));
-        balls.get(2).setSpeed(new Vector2(0, -200));
+        balls.get(2).setSpeed(new Vector2(-100, 200));
     }
 
     public void draw(SpriteBatch sb){
